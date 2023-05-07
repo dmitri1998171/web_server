9f33a850edfd11715facfac4a4f1266f43e61675
user <user@users-MacBook-Pro.local>
2023-05-08 01:29:27 +0600

    Rewrite HTTP-server

--- src/server.c
+++ src/server.c
@@ -1,183 +1,95 @@
+// webserver.c
+#include <arpa/inet.h>
+#include <errno.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <pthread.h>
 #include <unistd.h>
 
-#define PORT 56000
-#define MSG_SIZE 1024
+#define PORT 8080
+#define BUFFER_SIZE 1024
 
-char * filename = "/Users/user/Programming/HTTP-server/text.html";
-char * requestStatus = "HTTP/1.1 200 OK\r\n";
+int main() {
+    char buffer[BUFFER_SIZE];
+    char resp[] = "HTTP/1.0 200 OK\r\n"
+                  "Server: webserver-c\r\n"
+                  "Content-type: text/html\r\n\r\n"
+                  "<html>hello, world</html>\r\n";
 
-struct conectClient {
-	int newSocket;
-	pthread_t thread;
-};
-
-struct sockaddr_in serverAddr, newAddr;
-typedef struct { char *name, *value; } header_t;
-static header_t reqhdr[17] = { {"\0", "\0"} };
-
-// get request header
-char *request_header(const char* name)
-{
-    int i = 0;
-    header_t *h = reqhdr;
-    while(h->name) {
-        if (strcmp(h->name, name) == 0) return h->value;
-        h++;
+    // Create a socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("webserver (socket)");
+        return 1;
     }
-    return NULL;
-}
+    printf("socket created successfully\n");
 
-void respond(int socket)
-{
-    int rcvd, fd, bytes_read;
-    char *ptr;
+    // Create the address to bind the socket to
+    struct sockaddr_in host_addr;
+    int host_addrlen = sizeof(host_addr);
 
-    char* buf = malloc(65535);
-    rcvd=recv(socket , buf, 65535, 0);
+    host_addr.sin_family = AF_INET;
+    host_addr.sin_port = htons(PORT);
+    host_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    if (rcvd<0)    // receive error
-        fprintf(stderr,("recv() error\n"));
-    else if (rcvd==0)    // receive socket closed
-        fprintf(stderr,"Client disconnected upexpectedly.\n");
-    else    // message received
-    {
-        buf[rcvd] = '\0';
+    // Create client address
+    struct sockaddr_in client_addr;
+    int client_addrlen = sizeof(client_addr);
 
-        char* method = strtok(buf,  " \t\r\n");
-        char* uri    = strtok(NULL, " \t");
-     	char* prot   = strtok(NULL, " \t\r\n"); 
-		char* qs;
+    // Bind the socket to the address
+    if (bind(sockfd, (struct sockaddr *)&host_addr, host_addrlen) != 0) {
+        perror("webserver (bind)");
+        return 1;
+    }
+    printf("socket successfully bound to address\n");
 
-        fprintf(stderr, "\x1b[32m + [%s] %s\x1b[0m\n", method, uri);
-        
-        if (qs = strchr(uri, '?'))
-        {
-            *qs++ = '\0'; //split URI
-        } else {
-            qs = uri - 1; //use an empty string
+    // Listen for incoming connections
+    if (listen(sockfd, SOMAXCONN) != 0) {
+        perror("webserver (listen)");
+        return 1;
+    }
+    printf("server listening for connections\n");
+
+    for (;;) {
+        // Accept incoming connections
+        int newsockfd = accept(sockfd, (struct sockaddr *)&host_addr,
+                               (socklen_t *)&host_addrlen);
+        if (newsockfd < 0) {
+            perror("webserver (accept)");
+            continue;
+        }
+        printf("connection accepted\n");
+
+        // Get client address
+        int sockn = getsockname(newsockfd, (struct sockaddr *)&client_addr,
+                                (socklen_t *)&client_addrlen);
+        if (sockn < 0) {
+            perror("webserver (getsockname)");
+            continue;
         }
 
-        header_t *h = reqhdr;
-        char *t, *t2;
-        while(h < reqhdr+16) {
-            char *k,*v,*t;
-            k = strtok(NULL, "\r\n: \t"); 
-            if (!k) break;
-            v = strtok(NULL, "\r\n");     
-            while(*v && *v==' ') v++;
-            h->name  = k;
-            h->value = v;
-            h++;
-            fprintf(stderr, "[H] %s: %s\n", k, v);
-            t = v + 1 + strlen(v);
-            if (t[1] == '\r' && t[2] == '\n') break;
+        // Read from the socket
+        int valread = read(newsockfd, buffer, BUFFER_SIZE);
+        if (valread < 0) {
+            perror("webserver (read)");
+            continue;
         }
-        t++; // now the *t shall be the beginning of user payload
-        t2 = request_header("Content-Length"); // and the related header if there is  
-        char* payload = t;
-        int payload_size = t2 ? atol(t2) : (rcvd-(t-buf));
 
-		char* sendd = "HTTP/1.1 200 OK\r\n\r\n Hello! You are using ";
-        // call router
-        send(socket, sendd, strlen(sendd), 0);
+        // Read the request
+        char method[BUFFER_SIZE], uri[BUFFER_SIZE], version[BUFFER_SIZE];
+        sscanf(buffer, "%s %s %s", method, uri, version);
+        printf("[%s:%u] %s %s %s\n", inet_ntoa(client_addr.sin_addr),
+               ntohs(client_addr.sin_port), method, version, uri);
 
-		// printf("HTTP/1.1 200 OK\r\n\r\n");
-        // printf("Hello! You are using %s", request_header("User-Agent"));
-	}
-    close(socket);
-}
+        // Write to the socket
+        int valwrite = write(newsockfd, resp, strlen(resp));
+        if (valwrite < 0) {
+            perror("webserver (write)");
+            continue;
+        }
 
-
-// _________________________________________
-
-void *newSock(void *arg) {
-	struct conectClient *links = (struct conectClient*) arg;
-	
-	pthread_detach(links->thread);
-
-	respond(links->newSocket);
-	request_header("User-Agent");
-
-	// send(links->newSocket, requestStatus , strlen(buffer), 0);
-	
-	// FILE *fp = fopen(filename, "r");
-	// if (fp == 0)
-	// {
-	// 	printf("ERROR! hmtl not found!");
-	// 	exit(1);
-	// }
-		
-	// int bytes_read = 0;
-	// while( (fgets(buffer, 256, fp)) != NULL ) {
-	// 	printf("%s, bytes_read: \n", buffer );
-	// }
-	// fclose(fp);
-
-	// send(links->newSocket, buffer , strlen(buffer), 0);
-	
-	// }
-
-	printf("[+]Closing the connection.\n");
-	return NULL;
-}
-
-int main(){
-	int sockfd;
-	socklen_t structLength;
-
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("Erorr: socket creation failed. \n");
-        exit(1);
+        close(newsockfd);
     }
 
-	const int enable = 1;
-	// Позволяет выставить флаги для сокета SO_REUSEADDR позволяет множеству экземпляров одного и того же сервера запускаться на одном и том же порте
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0){
-    	printf("Setsockopt(SO_REUSEADDR) failed. \n");
-		exit(1);
-	}
-    
-	// Заполняет нулями
-	bzero(&serverAddr, sizeof(serverAddr));
-
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	// serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	serverAddr.sin_port = htons(PORT);
-
-	// Связывает сокет с локальным адресом протокола
-	if(bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-		printf("Erorr: bind failed. \n");
-		exit(1);
-	}
-
-	// Принимать входящие соединения и задет размер очереди
-	if(listen(sockfd, 5) < 0) {
-		printf("Erorr: listen failed. \n");
-		exit(1);
-	}
-	
-	structLength = sizeof(newAddr);
-
-	while (1) {
-		//	Динамическая стркутура через malloc
-		struct conectClient *links = malloc(sizeof(struct conectClient));
-		
-		links->newSocket = accept(sockfd, (struct sockaddr*)&newAddr, &structLength);
-	
-		if(pthread_create(&links->thread, NULL, *newSock, (void*) links) < 0){
-			printf("Erorr: thread create failed. \n");
-			exit(1);
-		}
-	}
-	
-	return 0;
+    return 0;
 }
